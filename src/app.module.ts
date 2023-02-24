919552086f9713bb3de9ae6f4edc4521d14e9f37
arlenmrx <122432501+arlenmrx@users.noreply.github.com>
2023-02-24 19:22:59 +0600

    Feat: setup project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { MessageModule } from './message/message.module';
+import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'keiz',
+      database: 'instagram',
+      entities: [],
+      synchronize: true,
+    }),
+    AuthModule,
+    MessageModule, 
+    PostModule, 
+    UserModule],
   controllers: [],
   providers: [],
+  
 })
 export class AppModule {}
